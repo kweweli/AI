80402eb97b07861b5af983f0814126b806328ac7
Kweweli Togba-Doya <56945594+escha2019@users.noreply.github.com>
2020-05-08 12:41:40 -0500

    clean code

--- Simple Neural Network Classifier.py
+++ Simple Neural Network Classifier.py
@@ -2,10 +2,6 @@
 # coding: utf-8
 
 # ## Dependencies
-
-# In[3]:
-
-
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -20,9 +16,6 @@ from sklearn.metrics import roc_curve
 
 
 # ## Functions
-
-# In[4]:
-
 
 def graphLearningCurve(title, loss, cv_loss, epoch, ylim=None):
     """Generate a simple plot of the validation and training learning curve"""
@@ -122,9 +115,6 @@ def buid_train_model(X,
 # Read data, slice predictors and label, and transform label to 1's and 0's
 # Do a train_test_split of 10%
 
-# In[5]:
-
-
 # read file and segment target and predicts
 data = pd.read_csv('cellDNA.csv', header = None)
 
@@ -143,9 +133,6 @@ X_test_norm           = normalized.transform(X_test)
 # ## Compute common statistics and class sizes
 # Class is highly imbalance
 
-# In[6]:
-
-
 print('Interesting:     ', len(y[y==1]))            # Count of bacteria that are worth studying
 print('Not Interesting: ', len(y[y==0]))
 
@@ -154,10 +141,10 @@ display(X.describe())                               # basic stats of predictors
 
 # ## Build Model and Evaluate Learning Curve.
 # Added 'class_weight' parameter to the model to account for high class imbalance. 
-# The interesting class (i.e., the bacterium worth studying or 1) is very small as compared to the other class so it is given a weight of 2. In other words, one data point from class 1 is worth twice from class 0. I tried different values for epochs and 35 seems to be the best in tracking the validation loss (at least for the current model structure). A natural extension might be to try different model structure.
-
-# In[12]:
-
+# The interesting class (i.e., the bacterium worth studying or 1) is very small as compared to the other class so it is 
+# given a weight of 2. In other words, one data point from class 1 is worth twice from class 0. I tried different values 
+# for epochs and 35 seems to be the best in tracking the validation loss (at least for the current model structure). 
+# A natural extension might be to try different model structure.
 
 epoch = 15
 
@@ -173,20 +160,12 @@ graphLearningCurve("NN Learning Curve", loss, cv_loss, epoch, ylim=None)       #
 
 
 # # Loss on Test Set
-
-# In[13]:
-
-
 print('Loss on Test Set: ', model.evaluate(X_test_norm, y_test))
 pred  = model.predict_classes(X_test_norm).ravel()               # predict on test set
 
 
 # # Metrics
 # The model has an accuracy 94%, precision of 73% and Recall of 94%. 
-
-# In[14]:
-
-
 # dict of metrics
 metrics_dict = {'recall': recall_score(y_test, pred), 'precision': precision_score(y_test, pred),
                'accuracy': accuracy_score(y_test, pred)}
@@ -195,10 +174,6 @@ metrics_dict = {'recall': recall_score(y_test, pred), 'precision': precision_sco
 print('Recall:    ', metrics_dict['recall'])
 print('Precision: ', metrics_dict['precision'])
 print('Accuracy:  ', metrics_dict['accuracy'])
-
-
-# In[15]:
-
 
 # Plot roc cure
 plot_roc("Feedforward NN Roc Curve", y_test, pred )
